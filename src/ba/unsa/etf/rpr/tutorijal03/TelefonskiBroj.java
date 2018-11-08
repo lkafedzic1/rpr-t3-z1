dca3e5e2b6fa9ca67c12124e5f99e77ec0b1eb63
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2018-11-08 07:45:46 +0100

    Sitna ispravka, dodano override

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,7 +1,8 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
-    String broj;
     public abstract String ispisi();
+
+    @Override
     public abstract int hashCode();
 }
